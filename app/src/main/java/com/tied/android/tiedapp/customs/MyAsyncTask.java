6db6add97008bd4e801217e2cf69b0216df8ca88
Emmanuel <nondefyde@gmail.com>
2016-06-03 21:33:35 +0100

    signup process

---
+++ app/src/main/java/com/tied/android/tiedapp/customs/MyAsyncTask.java
@@ -0,0 +1,35 @@
+package com.tied.android.tiedapp.customs;
+
+import android.os.AsyncTask;
+import android.os.Build;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by FEMI on 11/22/2015.
+ */
+public class MyAsyncTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+    public void execute() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            int corePoolSize = 15;
+            int maximumPoolSize = 20;
+            int keepAliveTime = 40;
+
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(maximumPoolSize);
+            Executor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue);
+            executeOnExecutor(threadPoolExecutor);
+
+        } else {
+            execute("");
+        }
+    }
+}
