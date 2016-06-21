344c9f63121b524dfc595bba966c7d2453cae5cb
Emmanuel <nondefyde@gmail.com>
2016-06-21 20:45:47 +0100

    scheduleA-update

--- app/src/main/java/com/tied/android/tiedapp/ui/activities/SwipeActivity.java
+++ app/src/main/java/com/tied/android/tiedapp/ui/activities/SwipeActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import com.tied.android.tiedapp.R;
 import com.tied.android.tiedapp.ui.adapters.PagerAdapter;
-import com.tied.android.tiedapp.util.ScheduleDialog;
 
 public class SwipeActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -29,8 +28,6 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         if (vpPager != null) {
             vpPager.setAdapter(adapterViewPager);
         }
-        ScheduleDialog alert = new ScheduleDialog();
-        alert.showDialog(this);
     }
 
     public void initComponent(){
---
+++ app/src/main/java/com/tied/android/tiedapp/ui/activities/TestActivity.java
@@ -0,0 +1,19 @@
+package com.tied.android.tiedapp.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tied.android.tiedapp.R;
+import com.tied.android.tiedapp.util.ScheduleDialog;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_schedule_clients_suggestion);
+
+        ScheduleDialog alert = new ScheduleDialog();
+        alert.showDialog(this);
+    }
+}
